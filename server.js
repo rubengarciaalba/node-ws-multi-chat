56cbdadba453aa5d7f6d0b6cc4a4240cbf296de1
Rubén García Alba <rubengarciaalba@Rubens-MacBook-Pro.local>
2021-05-19 00:21:50 +0200

    First commit

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ const WebSocket = require('ws');
 
 const port = 6969;
 const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
-})
+	ws.on('message', function incoming(data) {
 
-server.listen(port, function() {
-  console.log(`Server is listening on ${port}!`)
-})
+		console.log(data);
 
+		wss.clients.forEach(function each(client) {
+			console.log(client);
+
+			if(data === 'batteryLevel') {
+				client.send('96%'); // return the battery level
+			} else {
+				client.send('Message from websocket server');
+			}			
+		});
+	});
+});
+
+server.listen(port, function () {
+	console.log(`Server is listening on ${port}!`);
+});
